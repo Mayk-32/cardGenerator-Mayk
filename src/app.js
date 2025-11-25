4996f27e0c36e6a86675a4f551fee7f78f61d527
Aykut <m.aykutarslan1@gmail.com>
2025-11-25 00:55:00 +0000

    Random card genearator done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
 import "bootstrap";
 import "./style.css";
 
+  
+const suits = [
+{ symbol: "♠", class: "spade"},
+{ symbol: "♥", class: "heart"},
+{ symbol: "♦", class: "diamond"},
+{ symbol: "♣", class: "club"}
+];  
+const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const cardDiv = document.getElementById("card");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function randomCard() {
+  const suitObj = suits[Math.floor(Math.random() * suits.length)];
+  const value = values[Math.floor(Math.random() * values.length)];
+  return { value, suitObj };
+}
+
+function displayRandomCard() {
+  const card = randomCard();
+  cardDiv.className = "card";
+  cardDiv.classList.add(card.suitObj.class);
+  cardDiv.querySelector(".bottom-right").textContent = card.suitObj.symbol;
+  cardDiv.querySelector(".top-left").textContent = card.suitObj.symbol;
+  cardDiv.querySelector(".value").textContent = card.value;
+}
+
+window.addEventListener("DOMContentLoaded", displayRandomCard);
+
+document.getElementById("drawBtn").addEventListener("click", displayRandomCard);
